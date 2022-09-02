077591e2b77d2cfa2b1e43078ffdbf2988ab2d86
George <georgecbrainard4@gmail.com>
2022-09-01 23:29:07 -0400

    dom-todo-lab finished

--- js/app.js
+++ js/app.js
@@ -1,25 +1,13 @@
-const input = document.getElementById('#input')
-//console.log(inputElement)
+const input = document.getElementById('input')
 const btn = document.querySelector('#submit-button')
-//console.dir(btn)
 const ulist = document.querySelector('#todo-list')
-//console.log(ulElement)
 const resetBtn = document.getElementById('#reset button')
-//console.log(resetBtn)
 
-btn.addEventListener('click', function(evt) {
-    console.log(evt)
-//     btn.addEventListener('click', function(evt){
-// }) 
-btn.addEventListener('click', function(evt) {
-    const li = document.createElement('li')
-    console.log(li)
-  });
-btn.addEventListener('click', function(evt) {
-    const li = document.createElement('li')
-    const inp = document.querySelector('input')
-    li.textContent = inp.value
-   
-  document.querySelector('ul').appendChild(li)
-});
-});
+btn.addEventListener('click', function (evt) {
+  const li = document.createElement('li')
+  
+  li.textContent = input.value
+  
+  ulist.appendChild(li)
+  input.value = ''
+})
